e3290747c3e7d832d4b43993e7d29effd2abe185
Abanoub-G <ag14622@bristol.ac.uk>
2023-04-07 01:33:13 +0100

    Fixed issue with loading quantised model

--- utils/model.py
+++ utils/model.py
@@ -13,7 +13,6 @@ import time
 from methods.common import QuantizedNN
 import copy
 
-
 def create_model(model_dir, model_choice, model_variant, num_classes=10):
     model_module_path = model_dir+"/"+model_choice+".py"
     model_module      = importlib.util.spec_from_file_location("",model_module_path).loader.load_module()
@@ -33,6 +32,7 @@ def save_model(model, model_dir, model_filename):
     torch.save(model.state_dict(), model_filepath)
 
 def model_selection(model_selection_flag=0, model_dir="", model_choice="", model_variant="", saved_model_filepath="",num_classes=1, device=""):
+    
     if model_selection_flag == 0:
         # Create an untrained model.
         model = create_model(model_dir, model_choice, model_variant, num_classes)
@@ -42,68 +42,50 @@ def model_selection(model_selection_flag=0, model_dir="", model_choice="", model
         model = torch.hub.load('pytorch/vision:v0.10.0', model_variant, pretrained=True)
     
     elif model_selection_flag == 2:
-        
-        model = create_model(model_dir, model_choice, model_variant, num_classes)
         try:
             # Load a local pretrained model.
+            model = create_model(model_dir, model_choice, model_variant, num_classes)
             model = load_model(model=model, model_filepath=saved_model_filepath, device=device)
-            # print("Not a Quantised model")
         except:
-            STOPPED HERE
-            # print(saved_model_filepath)
-            model = torch.load(saved_model_filepath)
-            quantize it without calibration (weights will not be final)
-            model.train()
-            model.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm')
-            #model_fp32_fused = torch.quantization.fuse_modules(model,[['conv1', 'bn1', 'relu']])
-            model_fp32_prepared = torch.quantization.prepare_qat(model)
-            model_int8 = torch.quantization.convert(model_fp32_prepared)
+            # If above fails then the model is quantised, and it has go through the same process of quantisation then load the state dictionary of values.
+            model = create_model(model_dir, model_choice, model_variant, num_classes)
+
+            # Move the model to CPU since static quantization does not support CUDA currently.
+            cpu_device = torch.device("cpu:0")
+            model.to(cpu_device)
+
+            # Make a copy of the model.
+            fused_model = copy.deepcopy(model)
+
+            # The model has to be switched to evaluation mode for quantisation.
+            fused_model.eval()
+
+            # Fusing layers imporves speed and accuracy quantized model performance. https://pytorch.org/blog/quantization-in-practice/
+            # If model can be fused, fuse it.
+            try: 
+                fused_model = torch.quantization.fuse_modules(fused_model, [["conv1", "bn1", "relu"]], inplace=True)
+                for module_name, module in fused_model.named_children():
+                    if "layer" in module_name:
+                        for basic_block_name, basic_block in module.named_children():
+                            torch.quantization.fuse_modules(basic_block, [["conv1", "bn1", "relu1"], ["conv2", "bn2"]], inplace=True)
+                            for sub_block_name, sub_block in basic_block.named_children():
+                                if sub_block_name == "downsample":
+                                    torch.quantization.fuse_modules(sub_block, [["0", "1"]], inplace=True)
+            except:
+                # Model did not fuse, so continuing without fusing.
+                pass
+
+
+            compressed_model = QuantizedNN(model_fp32=fused_model)
+            quantization_config = torch.quantization.get_default_qconfig("fbgemm")
+            compressed_model.qconfig = quantization_config
+            torch.quantization.prepare(compressed_model, inplace=True)
+            compressed_model = torch.quantization.convert(compressed_model, inplace=True)
+            compressed_model.train()
 
             # load the real state dict
-            model_int8.load_state_dict(torch.load(saved_model_filepath))
-            return model_int8
-
-            # model = create_model(model_dir, model_choice, model_variant, num_classes)
-            # print("Quantised model")
-            # # if model is qunatised then the above will fail. The below is suited to load a quantised model.
-            # # Move the model to CPU since static quantization does not support CUDA currently.
-            # cpu_device = torch.device("cpu:0")
-            # model.to(cpu_device)
-
-            # # Make a copy of the model.
-            # fused_model = copy.deepcopy(model)
-
-            # # The model has to be switched to evaluation mode for quantisation.
-            # fused_model.eval()
-
-            
-            # # Fusing layers imporves speed and accuracy of Resnet quantized model performance. https://pytorch.org/blog/quantization-in-practice/
-            # # If model can be fused fuse it.Code below is set and tested for Resnet16.
-            # try: 
-            #     fused_model = torch.quantization.fuse_modules(fused_model, [["conv1", "bn1", "relu"]], inplace=True)
-            #     for module_name, module in fused_model.named_children():
-            #         if "layer" in module_name:
-            #             for basic_block_name, basic_block in module.named_children():
-            #                 torch.quantization.fuse_modules(basic_block, [["conv1", "bn1", "relu1"], ["conv2", "bn2"]], inplace=True)
-            #                 for sub_block_name, sub_block in basic_block.named_children():
-            #                     if sub_block_name == "downsample":
-            #                         torch.quantization.fuse_modules(sub_block, [["0", "1"]], inplace=True)
-            #     print("Model fused")
-            # except:
-            #     print("Model did not fuse, so continuing without fusing.")
-
-
-            # compressed_model = QuantizedNN(model_fp32=fused_model)
-            # quantization_config = torch.quantization.get_default_qconfig("fbgemm")
-            # compressed_model.qconfig = quantization_config
-            # torch.quantization.prepare(compressed_model, inplace=True)
-            
-            # # Use training data for calibration.
-            # # calibrate_model(model=compressed_model, loader=train_loader, device=cpu_device)
-            # compressed_model = torch.quantization.convert(compressed_model, inplace=True)
-
-            # compressed_model.train()
-            # model = load_model(model=model, model_filepath=saved_model_filepath, device=device)
+            compressed_model.load_state_dict(torch.load(saved_model_filepath))
+            return compressed_model
 
     model.to(device)
     return model
