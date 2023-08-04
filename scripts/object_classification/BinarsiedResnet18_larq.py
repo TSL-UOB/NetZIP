b732c8fd7745f51e12ad4037d5991f0db106161a
Abanoub-G <ag14622@bristol.ac.uk>
2023-08-04 02:17:55 +0100

    Progressing with larq experimentation.

--- scripts/object_classification/BinarsiedResnet18_larq.py
+++ scripts/object_classification/BinarsiedResnet18_larq.py
@@ -1,3 +1,4 @@
+# Guidance from this link: https://docs.larq.dev/zoo/tutorials/#evaluate-quicknet-with-tensorflow-datasets
 import os
 import sys
 
@@ -10,20 +11,44 @@ import larq_zoo as lqz
 from urllib.request import urlopen
 from PIL import Image
 
-img_path = "https://raw.githubusercontent.com/larq/zoo/master/tests/fixtures/elephant.jpg"
+def preprocess(data):
+    img = lqz.preprocess_input(data["image"])
+    label = tf.one_hot(data["label"], 1000)
+    return img, label 
 
-with urlopen(img_path) as f:
-    img = Image.open(f).resize((224, 224))
+dataset = (
+    tfds.load("imagenet2012:5.0.0", split=tfds.Split.VALIDATION)
+    .map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    .batch(128)
+    .prefetch(1)
+)
+# img_path = "https://raw.githubusercontent.com/larq/zoo/master/tests/fixtures/elephant.jpg"
 
-x = tf.keras.preprocessing.image.img_to_array(img)
-x = lqz.preprocess_input(x)
-x = np.expand_dims(x, axis=0)
+# with urlopen(img_path) as f:
+#     img = Image.open(f).resize((224, 224))
+
+# x = tf.keras.preprocessing.image.img_to_array(img)
+# x = lqz.preprocess_input(x)
+# x = np.expand_dims(x, axis=0)
 
 
 # model = lqz.sota.QuickNet(weights="imagenet")
 model = lqz.literature.BinaryResNetE18(weights="imagenet")
-preds = model.predict(x)
-print(lqz.decode_predictions(preds, top=5)[0])
+# model = lqz.literature.BinaryDenseNet45(weights="imagenet")
+# model = lqz.literature.RealToBinaryNet(weights="imagenet")
+# model = lqz.literature.XNORNet(weights="imagenet")
+
+# preds = model.predict(x)
+# print(lqz.decode_predictions(preds, top=5)[0])
+
+
+model.compile(
+    optimizer="sgd",
+    loss="categorical_crossentropy",
+    metrics=["categorical_accuracy", "top_k_categorical_accuracy"],
+)
+
+model.evaluate(dataset)
 
 
 # import psutil
